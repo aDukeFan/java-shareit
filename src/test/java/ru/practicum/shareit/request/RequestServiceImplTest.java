7e531002b568cc7e81c65653071048856c0bf3f3
aDukeFan <luckyloki666@ya.ru>
2024-07-06 21:15:49 +0400

    15.1.11 add request tests

--- src/test/java/ru/practicum/shareit/request/RequestServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/request/RequestServiceImplTest.java
@@ -10,12 +10,17 @@ import ru.practicum.shareit.item.ItemMapper;
 import ru.practicum.shareit.item.ItemMapperImpl;
 import ru.practicum.shareit.item.ItemRepository;
 import ru.practicum.shareit.request.dto.RequestDtoIncome;
+import ru.practicum.shareit.request.dto.RequestDtoWithItemList;
+import ru.practicum.shareit.request.model.Request;
 import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -56,10 +61,53 @@ public class RequestServiceImplTest {
     }
 
     @Test
-    public void getAllByRequesterIdTest() {
+    public void getByRequestIdTest() {
+        User user = new User()
+                .setId(1L)
+                .setName("Name")
+                .setEmail("ya@ya.ru");
+        Request firstRequest = new Request()
+                .setRequester(user)
+                .setCreated(LocalDateTime.now())
+                .setDescription("some needs")
+                .setId(1L);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(requestRepository.findById(1L)).thenReturn(Optional.of(firstRequest));
+        RequestDtoWithItemList expected = requestMapper.toSendAll(firstRequest);
+        assertEquals(expected, requestService.getByRequestId(1L, 1L));
+        verify(userRepository).findById(anyLong());
+        verify(requestRepository).findById(anyLong());
+    }
+
+    @Test
+    public void getAllByRequesterIdEmptyTest() {
         when(userRepository.findById(anyLong())).thenReturn(Optional.of(new User()));
         assertEquals(0, requestService.getAllByRequesterId(1L).size());
         verify(userRepository).findById(anyLong());
+        verify(requestRepository).findAllByRequesterId(anyLong());
+    }
+
+    @Test
+    public void getAllByRequesterIdTest() {
+        User user = new User()
+                .setId(1L)
+                .setName("Name")
+                .setEmail("ya@ya.ru");
+        Request firstRequest = new Request()
+                .setRequester(user)
+                .setCreated(LocalDateTime.now())
+                .setDescription("some needs");
+        Request second = new Request()
+                .setRequester(user)
+                .setCreated(LocalDateTime.now().minusDays(1))
+                .setDescription("another needs");
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        when(requestRepository.findAllByRequesterId(1L)).thenReturn(List.of(second, firstRequest));
+        List<RequestDtoWithItemList> result = requestService.getAllByRequesterId(1L);
+        assertEquals(2, result.size());
+        assertEquals(requestMapper.toSend(second).getDescription(),
+                result.get(1).getDescription());
+        verify(userRepository).findById(1L);
         verify(requestRepository).findAllByRequesterId(anyLong());
     }
 }
